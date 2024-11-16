8f24d13f08e18bb5e19f9d507985d551eb04fddc
Het Chawdagit config --global user.name Het <learner91047@gmail.com>
2024-11-16 17:44:00 +0530

    updated mongo_uri

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-      const conn = await mongoose.connect(`mongodb://localhost:27017/Createrrise`, {
-        useNewUrlParser: true,
-      });
-      console.log(`MongoDB Connected: {conn.connection.host}`);
-    } catch (error) {
-      console.error(error.message);
-      process.exit(1);
-    }
+  console.log(process.env.MONGO_URL);
+  try {
+    const conn = await mongoose.connect(`${process.env.MONGO_URL}`, {
+      useNewUrlParser: true,
+    });
+    console.log(`MongoDB Connected: {conn.connection.host}`);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
   }
+}
 
-  export default connectDB
+export default connectDB
